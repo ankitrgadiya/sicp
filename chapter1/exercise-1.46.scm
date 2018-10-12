e0aabea0c54e0e9076279ae9fc2a83af83f1b54d
Ankit R Gadiya <git@argp.in>
2018-10-12 20:00:21 +0530

    Exercise 1.46

---
+++ chapter1/exercise-1.46.scm
@@ -0,0 +1,38 @@
+; Exercise 1.46
+; Several of the numerical methods described in this chapter are instances of
+; an extremely general computational strategy known as iterative improvement.
+; Iterative improvement says that, to compute something, we start with an
+; initial guess for the answer, test if the guess is good enough, and otherwise
+; improve the guess and continue the process using the improved guess as the
+; new guess. Write a procedure iterative-improve that takes two procedures as
+; arguments: a method for telling whether a guess is good enough and a method
+; for improving a guess. iterative-improve should return as its value a
+; procedure that takes a guess as argument and keeps improving the guess until
+; it is good enough. Rewrite the sqrt procedure of Section 1.1.7 and the
+; fixed-point procedure of Section 1.3.3 in terms of iterative-improve.
+
+(define (iterative-improve check? improve)
+        (define (iter guess)
+                (if (check? guess)
+                    guess
+                    (iter (improve guess))))
+        iter)
+
+(define (average u v) (/ (+ u v) 2))
+
+(define (sqrt x)
+        ((iterative-improve
+          (lambda (y) (< (abs (- (square y) x)) 0.001))
+          (lambda (y) (average y (/ x y))))
+         x))
+
+(sqrt 2.0)
+
+(define (fixed-point f first-guess)
+        (define tolerance 0.00001)
+        ((iterative-improve
+          (lambda (y) (< (abs (- y (f y))) tolerance))
+          (lambda (y) (f y)))
+         first-guess))
+
+(fixed-point cos 1.0)
